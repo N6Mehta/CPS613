0a313f733dd3cb6bde0663935ec635f5bdf3d980
pkamboj <pkamboj@ENG203-15>
2016-11-13 14:31:54 -0500

    courseSearch form first commit - testing

---
+++ CPS613 RAMS THING/CPS613 RAMS THING/CourseSearch.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CourseSearch
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.searchButton = New System.Windows.Forms.Button()
+        Me.searchTextBox = New System.Windows.Forms.TextBox()
+        Me.searchListBox = New System.Windows.Forms.ListBox()
+        Me.cancelButton = New System.Windows.Forms.Button()
+        Me.doneButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'searchButton
+        '
+        Me.searchButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.searchButton.Location = New System.Drawing.Point(715, 44)
+        Me.searchButton.Name = "searchButton"
+        Me.searchButton.Size = New System.Drawing.Size(74, 29)
+        Me.searchButton.TabIndex = 0
+        Me.searchButton.Text = "Search"
+        Me.searchButton.UseVisualStyleBackColor = True
+        '
+        'searchTextBox
+        '
+        Me.searchTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.searchTextBox.Location = New System.Drawing.Point(27, 44)
+        Me.searchTextBox.Name = "searchTextBox"
+        Me.searchTextBox.Size = New System.Drawing.Size(682, 29)
+        Me.searchTextBox.TabIndex = 1
+        '
+        'searchListBox
+        '
+        Me.searchListBox.FormattingEnabled = True
+        Me.searchListBox.Location = New System.Drawing.Point(27, 201)
+        Me.searchListBox.Name = "searchListBox"
+        Me.searchListBox.ScrollAlwaysVisible = True
+        Me.searchListBox.Size = New System.Drawing.Size(682, 303)
+        Me.searchListBox.TabIndex = 2
+        '
+        'cancelButton
+        '
+        Me.cancelButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cancelButton.Location = New System.Drawing.Point(408, 537)
+        Me.cancelButton.Name = "cancelButton"
+        Me.cancelButton.Size = New System.Drawing.Size(125, 37)
+        Me.cancelButton.TabIndex = 3
+        Me.cancelButton.Text = "Cancel"
+        Me.cancelButton.UseVisualStyleBackColor = True
+        '
+        'doneButton
+        '
+        Me.doneButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.doneButton.Location = New System.Drawing.Point(244, 537)
+        Me.doneButton.Name = "doneButton"
+        Me.doneButton.Size = New System.Drawing.Size(123, 37)
+        Me.doneButton.TabIndex = 4
+        Me.doneButton.Text = "Done"
+        Me.doneButton.UseVisualStyleBackColor = True
+        '
+        'CourseSearch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(801, 586)
+        Me.Controls.Add(Me.doneButton)
+        Me.Controls.Add(Me.cancelButton)
+        Me.Controls.Add(Me.searchListBox)
+        Me.Controls.Add(Me.searchTextBox)
+        Me.Controls.Add(Me.searchButton)
+        Me.Name = "CourseSearch"
+        Me.Text = "Search For Courses "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents searchButton As Button
+    Friend WithEvents searchTextBox As TextBox
+    Friend WithEvents searchListBox As ListBox
+    Friend WithEvents cancelButton As Button
+    Friend WithEvents doneButton As Button
+End Class
---
+++ CPS613 RAMS THING/CPS613 RAMS THING/CourseSearch.vb
@@ -0,0 +1,13 @@
+﻿Public Class CourseSearch
+    Private Sub CourseSearch_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        searchListBox.Text = " Courses:"
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles searchTextBox.TextChanged
+
+    End Sub
+
+    Private Sub searchListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles searchListBox.SelectedIndexChanged
+
+    End Sub
+End Class
