0f03028de0238d61da1432f5d342b1f250f51011
Nitish Mehta <nitish.mehta33@gmail.com>
2016-11-13 14:40:57 -0500

    commit

--- CPS613 RAMS THING/CPS613 RAMS THING/Form1.Designer.vb
+++ CPS613 RAMS THING/CPS613 RAMS THING/Form1.Designer.vb
@@ -22,28 +22,81 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.CourseObject1 = New CPS613_RAMS_THING.CourseObject()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStripLabel2 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'CourseObject1
+        'ToolStrip1
         '
-        Me.CourseObject1.Location = New System.Drawing.Point(107, 36)
-        Me.CourseObject1.Name = "CourseObject1"
-        Me.CourseObject1.Size = New System.Drawing.Size(230, 120)
-        Me.CourseObject1.TabIndex = 1
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(90, 45)
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripLabel2, Me.ToolStripLabel1, Me.ToolStripSeparator1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1014, 52)
+        Me.ToolStrip1.TabIndex = 3
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton1.Image = Global.CPS613_RAMS_THING.My.Resources.Resources.logo_lpp_white_new__1_
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(94, 49)
+        Me.ToolStripButton1.Text = "ToolStripButton1"
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripLabel1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.ToolStripLabel1.IsLink = True
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(61, 49)
+        Me.ToolStripLabel1.Text = "Home"
+        '
+        'ToolStripLabel2
+        '
+        Me.ToolStripLabel2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripLabel2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripLabel2.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.ToolStripLabel2.IsLink = True
+        Me.ToolStripLabel2.Name = "ToolStripLabel2"
+        Me.ToolStripLabel2.Size = New System.Drawing.Size(82, 49)
+        Me.ToolStripLabel2.Text = "Sign Out"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 52)
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(676, 426)
-        Me.Controls.Add(Me.CourseObject1)
+        Me.ClientSize = New System.Drawing.Size(1014, 655)
+        Me.Controls.Add(Me.ToolStrip1)
         Me.IsMdiContainer = True
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Ryerson RAMS"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents CourseObject1 As CourseObject
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents ToolStripLabel2 As ToolStripLabel
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
 End Class
--- CPS613 RAMS THING/CPS613 RAMS THING/Form1.vb
+++ CPS613 RAMS THING/CPS613 RAMS THING/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
+    Private Sub ToolStripLabel2_Click(sender As Object, e As EventArgs) Handles ToolStripLabel2.Click
+        Me.Close()
+    End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+
+    End Sub
 End Class
