b67519febc1ed0eecb9b133df2edb092e7982eeb
pkamboj <pkamboj@ENG203-15>
2016-11-13 18:44:47 -0500

    did some stuff

--- CPS613 RAMS THING/CPS613 RAMS THING/CourseSearch.vb
+++ CPS613 RAMS THING/CPS613 RAMS THING/CourseSearch.vb
@@ -23,4 +23,8 @@
     Private Sub MCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles MCheckBox.CheckedChanged
         courseSelected = MCheckBox.Text
     End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
 End Class
