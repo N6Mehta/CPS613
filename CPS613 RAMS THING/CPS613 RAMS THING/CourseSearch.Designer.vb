fe17af70c256230c013794cf29cc221d2816b3bf
pkamboj <pkamboj@ENG203-15>
2016-11-13 16:52:41 -0500

    HOW YA DOIN

--- CPS613 RAMS THING/CPS613 RAMS THING/CourseSearch.Designer.vb
+++ CPS613 RAMS THING/CPS613 RAMS THING/CourseSearch.Designer.vb
@@ -35,6 +35,7 @@ Partial Class CourseSearch
         Me.FTCheckBox = New System.Windows.Forms.CheckBox()
         Me.WTCheckBox = New System.Windows.Forms.CheckBox()
         Me.courseListView = New System.Windows.Forms.ListView()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'searchButton
@@ -170,11 +171,21 @@ Partial Class CourseSearch
         Me.courseListView.TabIndex = 14
         Me.courseListView.UseCompatibleStateImageBehavior = False
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(139, 221)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Label1"
+        '
         'CourseSearch
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(801, 586)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.courseListView)
         Me.Controls.Add(Me.WTCheckBox)
         Me.Controls.Add(Me.FTCheckBox)
@@ -208,4 +219,5 @@ Partial Class CourseSearch
     Friend WithEvents FTCheckBox As CheckBox
     Friend WithEvents WTCheckBox As CheckBox
     Friend WithEvents courseListView As ListView
+    Friend WithEvents Label1 As Label
 End Class
