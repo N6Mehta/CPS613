c8fd685f40568b06e7b5a741e812f0d24c1532ec
pkamboj <pkamboj@ENG203-15>
2016-11-21 15:17:08 -0500

    dcsb,m

--- CPS613 RAMS THING/CPS613 RAMS THING/CourseSearch.vb
+++ CPS613 RAMS THING/CPS613 RAMS THING/CourseSearch.vb
@@ -4,7 +4,7 @@
 
         ' This call is required by the designer.
         InitializeComponent()
-        ' Add any initialization after the InitializeComponent() call.
+        ' Add any initialization after the InitializeComponent() call...
 
     End Sub
 
