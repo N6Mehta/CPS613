3f5c97e78040228b52301e038a7872a6899e1c9f
n6mehta <nitish.mehta33@gmail.com>
2016-11-20 23:10:39 -0500

    personal info page added

---
+++ CPS613 RAMS THING/CPS613 RAMS THING/Personal Information.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Personal_Information
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.homeStreet = New System.Windows.Forms.TextBox()
+        Me.homeCity = New System.Windows.Forms.TextBox()
+        Me.homeProvince = New System.Windows.Forms.TextBox()
+        Me.homeCode = New System.Windows.Forms.TextBox()
+        Me.mailCode = New System.Windows.Forms.TextBox()
+        Me.mailProvince = New System.Windows.Forms.TextBox()
+        Me.mailCity = New System.Windows.Forms.TextBox()
+        Me.mailStreet = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(137, 283)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(296, 283)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(57, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Home Address"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(57, 186)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Home Phone"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(319, 45)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(96, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Mailing Address"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(319, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(87, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Ryerson Email"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(57, 68)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Street"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(67, 90)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(24, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "City"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(42, 112)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(49, 13)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "Province"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(29, 134)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(64, 13)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "Postal Code"
+        '
+        'homeStreet
+        '
+        Me.homeStreet.Location = New System.Drawing.Point(99, 65)
+        Me.homeStreet.Name = "homeStreet"
+        Me.homeStreet.Size = New System.Drawing.Size(100, 20)
+        Me.homeStreet.TabIndex = 14
+        '
+        'homeCity
+        '
+        Me.homeCity.Location = New System.Drawing.Point(99, 87)
+        Me.homeCity.Name = "homeCity"
+        Me.homeCity.Size = New System.Drawing.Size(100, 20)
+        Me.homeCity.TabIndex = 15
+        '
+        'homeProvince
+        '
+        Me.homeProvince.Location = New System.Drawing.Point(99, 109)
+        Me.homeProvince.Name = "homeProvince"
+        Me.homeProvince.Size = New System.Drawing.Size(100, 20)
+        Me.homeProvince.TabIndex = 16
+        '
+        'homeCode
+        '
+        Me.homeCode.Location = New System.Drawing.Point(99, 131)
+        Me.homeCode.Name = "homeCode"
+        Me.homeCode.Size = New System.Drawing.Size(100, 20)
+        Me.homeCode.TabIndex = 17
+        '
+        'mailCode
+        '
+        Me.mailCode.Location = New System.Drawing.Point(363, 130)
+        Me.mailCode.Name = "mailCode"
+        Me.mailCode.Size = New System.Drawing.Size(100, 20)
+        Me.mailCode.TabIndex = 25
+        '
+        'mailProvince
+        '
+        Me.mailProvince.Location = New System.Drawing.Point(363, 108)
+        Me.mailProvince.Name = "mailProvince"
+        Me.mailProvince.Size = New System.Drawing.Size(100, 20)
+        Me.mailProvince.TabIndex = 24
+        '
+        'mailCity
+        '
+        Me.mailCity.Location = New System.Drawing.Point(363, 86)
+        Me.mailCity.Name = "mailCity"
+        Me.mailCity.Size = New System.Drawing.Size(100, 20)
+        Me.mailCity.TabIndex = 23
+        '
+        'mailStreet
+        '
+        Me.mailStreet.Location = New System.Drawing.Point(363, 64)
+        Me.mailStreet.Name = "mailStreet"
+        Me.mailStreet.Size = New System.Drawing.Size(100, 20)
+        Me.mailStreet.TabIndex = 22
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(293, 133)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(64, 13)
+        Me.Label13.TabIndex = 21
+        Me.Label13.Text = "Postal Code"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(306, 111)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(49, 13)
+        Me.Label14.TabIndex = 20
+        Me.Label14.Text = "Province"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(331, 89)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(24, 13)
+        Me.Label15.TabIndex = 19
+        Me.Label15.Text = "City"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(321, 67)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(35, 13)
+        Me.Label16.TabIndex = 18
+        Me.Label16.Text = "Street"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(46, 204)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 26
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(312, 204)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 27
+        '
+        'Personal_Information
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(510, 368)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.mailCode)
+        Me.Controls.Add(Me.mailProvince)
+        Me.Controls.Add(Me.mailCity)
+        Me.Controls.Add(Me.mailStreet)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.homeCode)
+        Me.Controls.Add(Me.homeProvince)
+        Me.Controls.Add(Me.homeCity)
+        Me.Controls.Add(Me.homeStreet)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Personal_Information"
+        Me.Text = "Personal_Information"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents homeStreet As TextBox
+    Friend WithEvents homeCity As TextBox
+    Friend WithEvents homeProvince As TextBox
+    Friend WithEvents homeCode As TextBox
+    Friend WithEvents mailCode As TextBox
+    Friend WithEvents mailProvince As TextBox
+    Friend WithEvents mailCity As TextBox
+    Friend WithEvents mailStreet As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+End Class
